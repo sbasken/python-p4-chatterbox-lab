5941cf67ca51bcb7da9002b317ef5a0baa002961
Saki <sbasken@gmail.com>
2023-04-05 08:57:57 -0700

    complete setting up model, Message, and build out a Flask API server

--- server/app.py
+++ server/app.py
@@ -14,13 +14,78 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+        messages_dict = [ message.to_dict() for message in messages]
+
+        response = make_response(
+            jsonify(messages_dict),
+            200
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    
+    elif request.method == 'POST':
+        # new_message = Message(
+        #     body = request.form.get('body'),
+        #     username = request.form.get('username')
+        # )
+        data = request.get_json()
+        new_message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        response = make_response(
+        jsonify(new_message.to_dict()),
+        201
+        )
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr,data[attr])
+            
+            db.session.add(message)
+            db.session.commit()
+
+            response = make_response(
+                jsonify(message.to_dict()),
+                200
+            )
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
+    elif request.method == 'DELETE':
+        if request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "Baked_goods deleted."
+            }
+            response = make_response(
+                jsonify(response_body),
+                200
+            )
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
